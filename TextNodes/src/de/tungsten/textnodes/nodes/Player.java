45bac14280159f434029493577d7201e2f5ba709
tungsten <WolframReinke@web.de>
2014-05-31 17:16:17 +0200

    added methods connect, disconnect and isConnected

--- TextNodes/src/de/tungsten/textnodes/nodes/Player.java
+++ TextNodes/src/de/tungsten/textnodes/nodes/Player.java
@@ -4,24 +4,40 @@ import de.tungsten.textnodes.connect.IConnection;
 
 public class Player extends Node {
 
-	private final IConnection connection;
-	
+	private IConnection connection;
+
 	public Player( IConnection connection ) {
-		
+
 		super( null, new String[] { "player" }, "It's the player!" );
 		this.connection = connection;
 	}
-	
+
 	@Override
 	public synchronized void move( Node target ) {
 
 		if ( target instanceof Room )
 			super.move( target );
 		else
-			throw new NodeException( getID(), "Players can only be moved to Rooms." );
+			throw new NodeException( getID(),
+					"Players can only be moved to Rooms." );
 	}
-	
+
+	public void connect( IConnection connection ) {
+		this.connection = connection;
+	}
+
+	public void disconnect() {
+		this.connection = null;
+	}
+
+	public boolean isConnected() {
+		return this.connection != null;
+	}
+
 	public boolean write( String message ) {
-		return connection.write( message );
+		if ( isConnected() )
+			return connection.write( message );
+		else
+			return false;
 	}
 }
