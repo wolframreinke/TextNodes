57696841df190a86adb59442ee1d383a8906f941
tungsten <WolframReinke@web.de>
2014-05-31 16:20:37 +0200

    renamed IMapProvider to IMapLoader

--- TextNodes/src/de/tungsten/textnodes/map/IMapProvider.java
+++
@@ -1,8 +0,0 @@
-package de.tungsten.textnodes.map;
-
-public interface IMapProvider {
-
-	public abstract boolean canLoad( String mapName );
-	
-	public abstract Map provideMap( String mapName ) throws Exception;
-}
